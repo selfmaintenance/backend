191745b2dc997cdd149b9a6a8d7d80ce657d042d
Edielson Rodrigues <edielson.silva@showtecnologia.com>
2024-09-06 00:18:19 -0300

    feat: start testes

---
+++ src/test/java/br/com/selfmaintenance/app/services/recurso/RecursoServiceTest.java
@@ -0,0 +1,30 @@
+package br.com.selfmaintenance.app.services.recurso;
+
+import org.junit.jupiter.api.Test;
+
+public class RecursoServiceTest {
+  @Test
+  void testBuscar() {
+
+  }
+
+  @Test
+  void testCriar() {
+
+  }
+
+  @Test
+  void testDeletar() {
+
+  }
+
+  @Test
+  void testEditar() {
+
+  }
+
+  @Test
+  void testListar() {
+
+  }
+}
