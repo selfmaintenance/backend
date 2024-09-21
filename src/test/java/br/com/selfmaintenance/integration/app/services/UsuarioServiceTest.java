6db66c9c35663c1ee0388eec73cf13ce74365af7
Edielson-Rodrigues <edielsonrodrigues567@gmail.com>
2024-09-21 14:44:23 -0300

    refactor: remover import

--- src/test/java/br/com/selfmaintenance/integration/app/services/UsuarioServiceTest.java
+++ src/test/java/br/com/selfmaintenance/integration/app/services/UsuarioServiceTest.java
@@ -1,7 +1,6 @@
 package br.com.selfmaintenance.integration.app.services;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
