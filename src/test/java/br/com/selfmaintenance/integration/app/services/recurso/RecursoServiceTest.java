db2e963b40c2f60ac985f1c920283cd04fd0220a
Edielson-Rodrigues <edielsonrodrigues567@gmail.com>
2024-09-22 12:11:56 -0300

    refactor: ajustar indentação

--- src/test/java/br/com/selfmaintenance/integration/app/services/recurso/RecursoServiceTest.java
+++ src/test/java/br/com/selfmaintenance/integration/app/services/recurso/RecursoServiceTest.java
@@ -78,102 +78,102 @@ class RecursoServiceTest {
 	@Test
 	@DisplayName("Deve buscar recurso por ID e email")
 	void testBuscarRecursoPorIdEEmail() throws ServiceException {
-	    // Criar um usuário e um recurso primeiro (reutilizando o código de criação)
-	    String email = this.faker.internet().emailAddress();
-	    UsuarioAutenticavelDTO usuarioAutenticavelDTO = new UsuarioAutenticavelDTO(
-	        this.faker.name().firstName(),
-	        email,
-	        this.faker.phoneNumber().cellPhone(),
-	        this.faker.internet().password(),
-	        UsuarioRole.OFICINA.getRole()
-	    );
-	    CriarUsuarioDTO criarUsuarioDTO = new CriarUsuarioDTO(
-	        usuarioAutenticavelDTO,
-	        null,
-	        GeradorDocumento.gerarCNPJ(),
-	        "M"
-	    );
-	    usuarioService.criar(criarUsuarioDTO);
-	    
-	    CriarRecursoDTO recursoDTO = new CriarRecursoDTO(
-				this.faker.name().name(), 
-				this.faker.number().numberBetween(0, 100), 
-				this.faker.lorem().sentence()
-			);
+		// Criar um usuário e um recurso primeiro (reutilizando o código de criação)
+		String email = this.faker.internet().emailAddress();
+		UsuarioAutenticavelDTO usuarioAutenticavelDTO = new UsuarioAutenticavelDTO(
+			this.faker.name().firstName(),
+			email,
+			this.faker.phoneNumber().cellPhone(),
+			this.faker.internet().password(),
+			UsuarioRole.OFICINA.getRole()
+		);
+		CriarUsuarioDTO criarUsuarioDTO = new CriarUsuarioDTO(
+			usuarioAutenticavelDTO,
+			null,
+			GeradorDocumento.gerarCNPJ(),
+			"M"
+		);
+		usuarioService.criar(criarUsuarioDTO);
+		
+		CriarRecursoDTO recursoDTO = new CriarRecursoDTO(
+			this.faker.name().name(), 
+			this.faker.number().numberBetween(0, 100), 
+			this.faker.lorem().sentence()
+		);
 
-	    Map<String, Long> respostaCriacao = recursoService.criar(recursoDTO, email);
-			Long idRecurso = respostaCriacao.get("idRecurso");
-			RecursoResponseDTO respostaMock = new RecursoResponseDTO(
-				idRecurso, 
-				recursoDTO.nome(), 
-				recursoDTO.descricao(), 
-				recursoDTO.quantidade()
-			);
+		Map<String, Long> respostaCriacao = recursoService.criar(recursoDTO, email);
+		Long idRecurso = respostaCriacao.get("idRecurso");
+		RecursoResponseDTO respostaMock = new RecursoResponseDTO(
+			idRecurso, 
+			recursoDTO.nome(), 
+			recursoDTO.descricao(), 
+			recursoDTO.quantidade()
+		);
 
-			// Act and assert
-	    RecursoResponseDTO recurso = recursoService.buscar(idRecurso, email);
-	    assertEquals(respostaMock, recurso);
+		// Act and assert
+		RecursoResponseDTO recurso = recursoService.buscar(idRecurso, email);
+		assertEquals(respostaMock, recurso);
 	}
 	
 	@Test
 	@DisplayName("Deve listar todos os recursos de um usuário")
 	void testListarRecursosDoUsuario() throws ServiceException {
-			// Arrange
-	    String email = this.faker.internet().emailAddress();
-	    UsuarioAutenticavelDTO usuarioAutenticavelDTO = new UsuarioAutenticavelDTO(
-	        this.faker.name().firstName(),
-	        email,
-	        this.faker.phoneNumber().cellPhone(),
-	        this.faker.internet().password(),
-	        UsuarioRole.OFICINA.getRole()
-	    );
-	    CriarUsuarioDTO criarUsuarioDTO = new CriarUsuarioDTO(
-	        usuarioAutenticavelDTO,
-	        null,
-	        GeradorDocumento.gerarCNPJ(),
-	        "M"
-	    );
-	    usuarioService.criar(criarUsuarioDTO);
-	    
-	    List<RecursoResponseDTO> respostaMock = new ArrayList<>();
-			CriarRecursoDTO recursoMock01 =  new CriarRecursoDTO(
-				this.faker.name().name(), 
-				this.faker.number().numberBetween(0, 100),
-				this.faker.lorem().sentence()
-			);
-	    Map<String, Long> mapIdRecurso01 = recursoService.criar(recursoMock01, email);
-	    
-			CriarRecursoDTO recursoMock02 =  new CriarRecursoDTO(
-				this.faker.name().name(), 
-				this.faker.number().numberBetween(0, 100),
-				this.faker.lorem().sentence()
-			);
-			Map<String, Long> mapIdRecurso02 = recursoService.criar(recursoMock02, email);
+		// Arrange
+		String email = this.faker.internet().emailAddress();
+		UsuarioAutenticavelDTO usuarioAutenticavelDTO = new UsuarioAutenticavelDTO(
+			this.faker.name().firstName(),
+			email,
+			this.faker.phoneNumber().cellPhone(),
+			this.faker.internet().password(),
+			UsuarioRole.OFICINA.getRole()
+		);
+		CriarUsuarioDTO criarUsuarioDTO = new CriarUsuarioDTO(
+			usuarioAutenticavelDTO,
+			null,
+			GeradorDocumento.gerarCNPJ(),
+			"M"
+		);
+		usuarioService.criar(criarUsuarioDTO);
+		
+		List<RecursoResponseDTO> respostaMock = new ArrayList<>();
+		CriarRecursoDTO recursoMock01 =  new CriarRecursoDTO(
+			this.faker.name().name(), 
+			this.faker.number().numberBetween(0, 100),
+			this.faker.lorem().sentence()
+		);
+		Map<String, Long> mapIdRecurso01 = recursoService.criar(recursoMock01, email);
+		
+		CriarRecursoDTO recursoMock02 =  new CriarRecursoDTO(
+			this.faker.name().name(), 
+			this.faker.number().numberBetween(0, 100),
+			this.faker.lorem().sentence()
+		);
+		Map<String, Long> mapIdRecurso02 = recursoService.criar(recursoMock02, email);
 
-			respostaMock.add(
-				new RecursoResponseDTO(
-					mapIdRecurso01.get("idRecurso"),
-					recursoMock01.nome(),
-					recursoMock01.descricao(),
-					recursoMock01.quantidade()
-				)
-			);
-			respostaMock.add(
-				new RecursoResponseDTO(
-					mapIdRecurso02.get("idRecurso"),
-					recursoMock02.nome(),
-					recursoMock02.descricao(),
-					recursoMock02.quantidade()
-				)
-			);
-	    // Act
-	    List<RecursoResponseDTO> recursos = recursoService.listar(email);
+		respostaMock.add(
+			new RecursoResponseDTO(
+				mapIdRecurso01.get("idRecurso"),
+				recursoMock01.nome(),
+				recursoMock01.descricao(),
+				recursoMock01.quantidade()
+			)
+		);
+		respostaMock.add(
+			new RecursoResponseDTO(
+				mapIdRecurso02.get("idRecurso"),
+				recursoMock02.nome(),
+				recursoMock02.descricao(),
+				recursoMock02.quantidade()
+			)
+		);
+		// Act
+		List<RecursoResponseDTO> recursos = recursoService.listar(email);
 
-	    // Assert
-	    assertAll("Verificar lista de recursos do usuário",
-	        () -> assertNotNull(recursos),
-	        () -> assertEquals(2, recursos.size()),
-					() -> assertEquals(respostaMock, recursos)
-	    );
+		// Assert
+		assertAll("Verificar lista de recursos do usuário",
+			() -> assertNotNull(recursos),
+			() -> assertEquals(2, recursos.size()),
+			() -> assertEquals(respostaMock, recursos)
+		);
 	}	
 }
